1197ab366c0058a6d0b51e7dabcfb50ab9b1b013
xifei <xifei@nfs.iscas.ac.cn>
2014-09-05 16:44:07 +0800

    add node_module dbus

---
+++ dbus-node/dbus/build/Release/.deps/Release/obj.target/dbus/src/object_handler.o.d
@@ -0,0 +1,60 @@
+cmd_Release/obj.target/dbus/src/object_handler.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DLIB_EXPAT=expat' '-DBUILDING_NODE_EXTENSION' -I/usr/include/nodejs/src -I/usr/include/nodejs/deps/uv/include -I/usr/include/nodejs/deps/v8/include  -Wall -Wextra -Wno-unused-parameter -pthread -m32 -std=gnu++0x -I/usr/include/dbus-1.0 -I/usr/lib/i386-linux-gnu/dbus-1.0/include -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/dbus/src/object_handler.o.d.raw   -c -o Release/obj.target/dbus/src/object_handler.o ../src/object_handler.cc
+Release/obj.target/dbus/src/object_handler.o: ../src/object_handler.cc \
+ /usr/include/nodejs/deps/v8/include/v8.h \
+ /usr/include/nodejs/deps/v8/include/v8stdint.h \
+ /usr/include/nodejs/src/node.h /usr/include/nodejs/deps/uv/include/uv.h \
+ /usr/include/nodejs/deps/uv/include/uv-private/uv-unix.h \
+ /usr/include/nodejs/deps/uv/include/uv-private/ngx-queue.h \
+ /usr/include/nodejs/deps/uv/include/uv-private/uv-linux.h \
+ /usr/include/nodejs/src/node_object_wrap.h \
+ /usr/include/nodejs/src/node.h /usr/include/dbus-1.0/dbus/dbus.h \
+ /usr/lib/i386-linux-gnu/dbus-1.0/include/dbus/dbus-arch-deps.h \
+ /usr/include/dbus-1.0/dbus/dbus-macros.h \
+ /usr/include/dbus-1.0/dbus/dbus-address.h \
+ /usr/include/dbus-1.0/dbus/dbus-types.h \
+ /usr/include/dbus-1.0/dbus/dbus-errors.h \
+ /usr/include/dbus-1.0/dbus/dbus-protocol.h \
+ /usr/include/dbus-1.0/dbus/dbus-bus.h \
+ /usr/include/dbus-1.0/dbus/dbus-connection.h \
+ /usr/include/dbus-1.0/dbus/dbus-memory.h \
+ /usr/include/dbus-1.0/dbus/dbus-message.h \
+ /usr/include/dbus-1.0/dbus/dbus-shared.h \
+ /usr/include/dbus-1.0/dbus/dbus-misc.h \
+ /usr/include/dbus-1.0/dbus/dbus-pending-call.h \
+ /usr/include/dbus-1.0/dbus/dbus-server.h \
+ /usr/include/dbus-1.0/dbus/dbus-signature.h \
+ /usr/include/dbus-1.0/dbus/dbus-syntax.h \
+ /usr/include/dbus-1.0/dbus/dbus-threads.h ../src/dbus.h ../src/decoder.h \
+ ../src/encoder.h ../src/object_handler.h
+../src/object_handler.cc:
+/usr/include/nodejs/deps/v8/include/v8.h:
+/usr/include/nodejs/deps/v8/include/v8stdint.h:
+/usr/include/nodejs/src/node.h:
+/usr/include/nodejs/deps/uv/include/uv.h:
+/usr/include/nodejs/deps/uv/include/uv-private/uv-unix.h:
+/usr/include/nodejs/deps/uv/include/uv-private/ngx-queue.h:
+/usr/include/nodejs/deps/uv/include/uv-private/uv-linux.h:
+/usr/include/nodejs/src/node_object_wrap.h:
+/usr/include/nodejs/src/node.h:
+/usr/include/dbus-1.0/dbus/dbus.h:
+/usr/lib/i386-linux-gnu/dbus-1.0/include/dbus/dbus-arch-deps.h:
+/usr/include/dbus-1.0/dbus/dbus-macros.h:
+/usr/include/dbus-1.0/dbus/dbus-address.h:
+/usr/include/dbus-1.0/dbus/dbus-types.h:
+/usr/include/dbus-1.0/dbus/dbus-errors.h:
+/usr/include/dbus-1.0/dbus/dbus-protocol.h:
+/usr/include/dbus-1.0/dbus/dbus-bus.h:
+/usr/include/dbus-1.0/dbus/dbus-connection.h:
+/usr/include/dbus-1.0/dbus/dbus-memory.h:
+/usr/include/dbus-1.0/dbus/dbus-message.h:
+/usr/include/dbus-1.0/dbus/dbus-shared.h:
+/usr/include/dbus-1.0/dbus/dbus-misc.h:
+/usr/include/dbus-1.0/dbus/dbus-pending-call.h:
+/usr/include/dbus-1.0/dbus/dbus-server.h:
+/usr/include/dbus-1.0/dbus/dbus-signature.h:
+/usr/include/dbus-1.0/dbus/dbus-syntax.h:
+/usr/include/dbus-1.0/dbus/dbus-threads.h:
+../src/dbus.h:
+../src/decoder.h:
+../src/encoder.h:
+../src/object_handler.h:
